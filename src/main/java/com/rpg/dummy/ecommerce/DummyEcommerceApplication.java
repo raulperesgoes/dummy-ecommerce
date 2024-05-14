e8ee5010e8dca04a76460fefab0d21ae901b1428
raulperesgoes <raulperesgoes@gmail.com>
2024-05-14 18:26:22 -0300

    dummy ecommerce project creation

---
+++ src/main/java/com/rpg/dummy/ecommerce/DummyEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.rpg.dummy.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpg/dummy/ecommerce/DummyEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpg.dummy.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
